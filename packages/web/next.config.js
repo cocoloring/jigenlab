65b91992b421ec5deb6cdf1260f24db7372aa809
cocoloring <cocoloring@outlook.com>
2021-12-12 20:43:28 +0100

    add transpile package for packing of esm packages

---
+++ packages/web/next.config.js
@@ -0,0 +1,9 @@
+const setModules = require('next-transpile-modules')
+
+const withModules = setModules([
+    // nothing to transpile yet
+])
+
+module.exports = withModules({
+    // nothing to setup yet
+})
